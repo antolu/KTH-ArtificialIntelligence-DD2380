f746e76bf34a80ae38c880fa95743955177916f5
Anton Lu <anton.lu@epfl.ch>
2018-09-25 14:11:51 +0200

    FInished TTT3D

--- A2/README.md
+++ A2/README.md
@@ -5,3 +5,7 @@
 
 2D Tic-Tac-Toe
 [3128717](https://kth.kattis.com/submissions/3128713)
+
+3D Tic-Tac-Toe
+
+[3130056](https://kth.kattis.com/submissions/3130056)
--- A2/TTT3D_cpp/player.hpp
+++ A2/TTT3D_cpp/player.hpp
@@ -13,11 +13,48 @@ namespace TICTACTOE3D
 class Player
 {
 public:
+    Player();
     ///perform a move
     ///\param pState the current state of the board
     ///\param pDue time before which we must have returned
     ///\return the next state the board is in after our move
     GameState play(const GameState &pState, const Deadline &pDue);
+
+private:
+    /**
+     * @param gamestate The current state we are analyzing
+     * @param player The current player
+     * 
+     * @return Returns a heuristic value that aproximates an utility
+     * function of the state.
+     */
+    int minimax(GameState & gamestate, int player, int depth);
+
+    /**
+     * @param gamestate The current state we are analyzing
+     * @param depth The depth of the current search tree
+     * @param alpha The current best value achievable by player X
+     * @param beta The current best value achievable by B
+     * @param player The current player
+     * 
+     * @return The minimax value of the state
+     */
+    int alphabeta(GameState & gamestate, int depth, int & alpha, int & beta, int player);
+
+    /**
+     * @param gamestate The current state we are analying
+     * 
+     * @return Returns the utility of this game state.
+     */
+    int utility(GameState & gamestate, int & depth);
+
+    uint8_t playerX;
+    uint8_t playerO;
+    const int me = 0;
+    const int opp = 1;
+
+    static int const winCombinations[76][4];
+    static int const rewards[5][5];
 };
 
 /*namespace TICTACTOE3D*/ }
